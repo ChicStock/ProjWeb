a99b65a476dc86706236ecc6f77e2b6d9571963a
biiancatorres <biancatorres@edu.unifor.br>
2025-09-22 16:47:35 -0300

    salvando alterações

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css'
-<<<<<<< HEAD
 import Navbar from './components/NavBar.jsx'
-=======
-import Cadastro from './pages/cadastro'
->>>>>>> af6aa18b5c89cd64b0f0db03b17c1e87153f5155
+import Cadastro from './pages/cadastro.jsx'
+
 
 function App() {
 
   return (
     <>
-     <Cadastro/> 
-     <>NavBar</>
+     <Navbar/>
     </>
   )
 }
